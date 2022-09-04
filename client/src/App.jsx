8c8a5ffeba81179059d350183b3705abacae2465
Dogukan Caltekin <dcaltekin@gmail.com>
2022-09-04 04:21:43 +0300

    connected-to-client-side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,47 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
+
 function App() {
-  return <div className="App"></div>;
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div>
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="Doğukan..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join the Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
 }
 
 export default App;
